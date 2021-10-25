b94987044ef6b6b3d9f3819805f6c93559e4fc24
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-10-25 15:32:17 +0600

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server')
+const { PubSub } = require('graphql-subscriptions');
+const mongoose =require('mongoose')
+
+const typeDefs = require('./graphQL/typeDefs')
+const resolvers = require('./graphQL/resolvers')
+const { MONGODB } = require('./config')
+
+const pubsub = new PubSub()
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({req}) => ({ req, pubsub })
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+.then(() => {
+    console.log('MongoDB Connected')
+    return server.listen({port: PORT})
+})
+.then(res => {
+    console.log(`Server running at ${res.url}`)
+})
+.catch(err => console.error(err))
